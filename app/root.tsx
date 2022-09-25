b99f6895da00f00484f31688883f56bc5c88afed
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2022-09-25 09:24:27 -0500

    Aver si

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 } from "@remix-run/react";
 import { MantineProvider, createEmotionCache } from "@mantine/core";
 import { StylesPlaceholder } from "@mantine/remix";
+import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -18,6 +19,9 @@ export const meta: MetaFunction = () => ({
 
 createEmotionCache({ key: "mantine" });
 
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 export default function App() {
   return (
     <MantineProvider theme={{}} withGlobalStyles withNormalizeCSS>
